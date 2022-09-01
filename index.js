875742f325fa878fcdb504a9defcc647e04de614
Abrar <abrarfahimr@gmail.com>
2022-08-31 23:43:15 -0400

    set up routes and main index

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pokemon = require('./routes/pokemon');
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+//CORS middleware
+app.use(cors());
+
+//Middleware to give access to req.body
+app.use(express.json());
+
+//give access to urlencoded data.
+app.use(express.urlencoded({ extended: true }));
+
+//Middleware to serve up static files
+app.use(express.static("public"));
+
+//route to all videos 
+app.use("/pokemon", pokemon);
+
+//startup the local server
+app.listen(PORT, () => {
+  console.log("Server is up and running")
+})
