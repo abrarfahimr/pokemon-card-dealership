bf0c045bd408fa880895e7dd198c82719de01f5d
Abrar <abrarfahimr@gmail.com>
2022-12-07 20:17:53 -0500

    added loading screen if api is slow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import './App.scss';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import Cart from'./components/pages/cartPage/cartPage';
-import Shopping from'./components/pages/shoppingPage/shoppingPage';
+import Cart from './components/pages/cartPage/cartPage';
+import Shopping from './components/pages/shoppingPage/shoppingPage';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
@@ -11,19 +11,19 @@ const API_URL = process.env.REACT_APP_API_URL;
 function App() {
   const [cards, setCards] = useState([]);
   const [cardId, setCardId] = useState([]);
-  const [cardInfo, setCardInfo] = useState([])
+  const [cardInfo, setCardInfo] = useState([]);
 
   useEffect(() => {
-  axios
-    .get(`${API_URL}/pokemon`)
-    .then((response) => {
-      setCards(response.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    axios
+      .get(`${API_URL}/pokemon`)
+      .then((response) => {
+        setCards(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
-  
+
   useEffect(() => {
     axios
       .get(`${API_URL}/pokemon/${cardId}`)
@@ -34,31 +34,40 @@ function App() {
         console.log(err);
       });
   }, [cardId]);
-    
-          
-  if (cards === null || cardId === null) {
-        return <main className="loading">Loading...</main>;
+
+  if (cards === [] || cardId === []) {
+    return <main className="loading">Loading...</main>;
+  } else {
+    return (
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Shopping
+                cards={cards}
+                setCards={setCards}
+                cardId={cardId}
+                setCardId={setCardId}
+              />
+            }
+          />
+          <Route
+            path="/cart"
+            element={
+              <Cart
+                cardId={cardId}
+                setCardId={setCardId}
+                cardInfo={cardInfo}
+                setCardInfo={setCardInfo}
+              />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
   }
-        
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Shopping
-          cards = {cards}
-          setCards = {setCards}
-          cardId={cardId}
-          setCardId = {setCardId}
-        />} />
-        <Route path="/cart" element={<Cart 
-          cardId={cardId}
-          setCardId = {setCardId}
-          cardInfo = {cardInfo}
-          setCardInfo = {setCardInfo}
-        />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
 
 export default App;
